6e29d812aaacad1cb42e0bc9072db66cce4e60c9
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-08-04 14:19:27 +0100

    solved problem for single binary array sort

---
+++ src/BinaryTreeSortingAccending.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    var intArray = intArrayOf(1,0,0,1,0,1,1,1,0,0,0)
+//    var intArray = intArrayOf(1,0,0,1)
+
+    // initialize min value to first element of an array
+    // for loop from second element of an array
+    // if first > second move swap the position
+
+    for (i in intArray.indices){//0,1,2,3
+        for (j in i+1 until intArray.size){//1,2,3
+            println("${intArray[i]}, ${intArray[j]}")
+            if (intArray[i] > intArray[j]){//1>0,
+                var temp = intArray[i]
+                intArray[i] = intArray[j]
+                intArray[j] = temp
+            }
+        }
+    }
+    println(intArray.contentToString())
+}
