f526b76334a49a6f9195a4d26b84aaaa280585f2
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-08-04 22:51:53 +0100

    solved binary single tree decending

--- src/BinaryTreeSortingDecending.kt
+++ src/BinaryTreeSortingDecending.kt
@@ -1,3 +1,14 @@
 fun main() {
-
+    var intArray = intArrayOf(1,0,0,1,0,1,1,1,0,0,0)
+    for (i in intArray.indices){//0,1,2,3
+        for (j in i+1 until intArray.size){//1,2,3
+            println("${intArray[i]}, ${intArray[j]}")
+            if (intArray[i] < intArray[j]){//1>0,
+                var temp = intArray[i]
+                intArray[i] = intArray[j]
+                intArray[j] = temp
+            }
+        }
+    }
+    println(intArray.contentToString())
 }
