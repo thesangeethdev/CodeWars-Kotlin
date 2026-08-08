daaa31d4857d59ab3fb8034c8d50d91c80231ccb
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-08-08 01:00:01 +0100

    solved 1d tree array to get min and max values

--- src/BinaryTreeMaxValue.kt
+++ src/BinaryTreeMaxValue.kt
@@ -1,5 +1,10 @@
 fun main() {
-
-
-
+    val binaryTree = intArrayOf(2, 5, 12, 8, 4,25)
+    var max = 0
+    binaryTree.forEach {
+        if (it>max){
+            max = it
+        }
+    }
+    println(max)
 }
--- src/BinaryTreeMinValue.kt
+++ src/BinaryTreeMinValue.kt
@@ -1,3 +1,10 @@
 fun main() {
-
+    val binaryTree = intArrayOf(2, 5, 12, 8, 4,25, 1)
+    var max = binaryTree[0]
+    binaryTree.forEach {
+        if (it<max){
+            max = it
+        }
+    }
+    println(max)
 }
