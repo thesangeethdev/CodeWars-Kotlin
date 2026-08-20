90d7034ee1052e025746058fc0306218acda4e65
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-08-21 00:36:43 +0100

    hashset removes duplicates so change the logic to use chararray

--- src/anagrams.kt
+++ src/anagrams.kt
@@ -1,17 +1,14 @@
 fun main() {
 
-    val str1 = "cats"
-    val str2 = "tocs"
+    val str1 = "paper"
+    val str2 = "reapa"
 
-    val hashSet1 = str1.toHashSet()
-    println(hashSet1)
-    val hashSet2 = str2.toHashSet()
-    println(hashSet2)
+    val charArray1 = str1.toCharArray()
+    println(charArray1)
+    val charArray2 = str2.toCharArray()
+    println(charArray2)
 
-    val substract = hashSet1.subtract(hashSet2)
-
-    println(substract)
-    if (substract.isEmpty()){
+    if (charArray1.contentEquals(charArray2)){
         println(true)
     }else{
         println(false)
