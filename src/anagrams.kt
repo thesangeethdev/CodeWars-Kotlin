e352a9df11521ff04c0a372070dd9434aeb3f833
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-08-21 01:14:03 +0100

    bug fix - tochar array also will not work switch to hashmap count char method

--- src/anagrams.kt
+++ src/anagrams.kt
@@ -1,14 +1,29 @@
-fun main() {
+fun main(){
 
-    val str1 = "cats"
-    val str2 = "tocs"
+    val str1 = "pp"
+    val str2 = "oo"
 
-    val charArray1 = str1.toCharArray().toSortedSet()
-    println(charArray1)
-    val charArray2 = str2.toCharArray().toSortedSet()
-    println(charArray2)
+    val hashMap1 = HashMap<Char, Int>()
+    val hashMap2 = HashMap<Char, Int>()
 
-    if (charArray1 == charArray2){
+    for (i in str1.toCharArray()){
+        if (hashMap1[i] == null){
+            hashMap1[i] = 0
+        }
+        hashMap1[i] = hashMap1[i]?.plus(1) ?: 0
+    }
+
+    for (i in str2.toCharArray()){
+        if (hashMap2[i] == null){
+            hashMap2[i] = 0
+        }
+        hashMap2[i] = hashMap2[i]?.plus(1) ?: 0
+    }
+
+    println(hashMap1)
+    println(hashMap2)
+
+    if (hashMap1 == hashMap2){
         println(true)
     }else{
         println(false)
