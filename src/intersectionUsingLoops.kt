c43aac90dae6fbe7f54b199a76acf76f3b412b75
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-08-28 11:48:35 +0100

    solved intersection using nested loops

--- src/intersectionUsingLoops.kt
+++ src/intersectionUsingLoops.kt
@@ -3,15 +3,17 @@ import java.util.stream.Collectors
 fun main() {
 
     val a = listOf(0,1,2)
-    val b = listOf(10,11)
+    val b = listOf(10,1,2)
+    val results = mutableListOf<Int>()
 //
-//    for (i in a.indices) {
-//        for (j in b.indices) {
-//            if (a[i] == a[j]) {
-//                print(listOf(a[i], b[j]))
-//            }
-//        }
-//    }
-    println(a.stream().filter { item -> b.contains(item) }.collect(Collectors.toList()))
+    for (i in a.indices) {
+        for (j in b.indices) {
+            if (a[i] == b[j]) {
+                results.add(i)
+            }
+        }
+    }
+    println(results)
+//    println(a.stream().filter { item -> b.contains(item) }.collect(Collectors.toList()))
 
 }
