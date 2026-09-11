92e9b1e985c8c2d3425c9038d5e777cd8ff1b67e
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-09-11 01:06:48 +0100

    solved using linkedlist to return list using recursive function

--- src/LinkedListValues.kt
+++ src/LinkedListValues.kt
@@ -1,4 +1,4 @@
-import Source.linkedListValues
+import Source.linkedListValuesRecursive
 
 class Node<T>(var `val`: T) {
     var next: Node<T>? = null
@@ -15,12 +15,28 @@ object Source {
         return list
     }
 
+    fun linkedListValuesRecursive(head: Node<String>?): List<String> {
+        val list = mutableListOf<String>()
+        val current : Node<String>? = head
+
+        if (current == null) return emptyList()
+        list.add(current.`val`)
+        return list + linkedListValuesRecursive(current.next)
+    }
+
 
 }
 
 fun main() {
 
-    println(linkedListValues(null))
+    val a = Node<String>("a")
+    val b = Node<String>("b")
+    val c = Node<String>("c")
+    val d = Node<String>("d")
+    a.next = b
+    b.next = c
+    c.next = d
+    println(linkedListValuesRecursive(a))
     // this function behaves as `main()` for the 'run' command
     // you may sandbox in this function, but should not remove it
 }
